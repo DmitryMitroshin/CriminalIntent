07c89f55854e73f577a2bf1aad66191510aa19be
Dmitry <mitroshin.dev@gmail.com>
2016-10-07 17:49:13 +0300

    Создан хост для нового фрагмента, в котором будет отображаться список.

---
+++ app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.devgmail.mitroshin.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Dmitry on 07.10.2016.
+ */
+public class CrimeListFragment extends Fragment {
+}
