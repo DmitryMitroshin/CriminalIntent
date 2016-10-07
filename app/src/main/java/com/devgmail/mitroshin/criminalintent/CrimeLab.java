86376110211d3d6b298deb8c3bee8d20f1699906
Dmitry <mitroshin.dev@gmail.com>
2016-10-07 13:17:07 +0300

    Создан и заполнен тестовыми данными список объектов Crime

--- app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeLab.java
+++ app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeLab.java
@@ -2,12 +2,19 @@ package com.devgmail.mitroshin.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Dmitry on 07.10.2016.
  */
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
+    // Список, для хранения объектов Crime
+    private List<Crime> mCrimes;
 
     // Синглейтный класс. Создает объект, если тот ранее не существовал.
     // Возвращает экземпляр, если тот уже имеется.
@@ -20,6 +27,27 @@ public class CrimeLab {
 
     // Конструктор синглетного класса должен быть закрытым
     private CrimeLab (Context context) {
+        mCrimes = new ArrayList<>();
+        // Заполняем массив для примера
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Каждый четный будет раскрыт
+            mCrimes.add(crime);
+        }
+    }
 
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    // Возвращает конкретный элемент списка по идентификатору
+    public Crime getCrime (UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 }
