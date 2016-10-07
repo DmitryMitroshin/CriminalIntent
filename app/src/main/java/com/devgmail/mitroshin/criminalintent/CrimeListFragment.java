a708ce5b7069c3feff9528817fcd75dfe8709bb2
Dmitry <mitroshin.dev@gmail.com>
2016-10-07 23:42:05 +0300

    Для списка реализован Holder и Adapter.

--- app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeListFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Dmitry on 07.10.2016.
@@ -24,5 +27,45 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        // Вызывается RecyclerView , когда требуется новое представление для отображения
+        // Создаем View и упаковываем его в ViewHolder
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            // Заполнение макета из стандартной библиотеки Android. Этот макет содержит только TextView
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeHolder(view);
+        }
+
+        // Связываает представление View объекта ViewHolder  с объектом модели
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.mTitleTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
     }
 }
