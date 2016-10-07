a6dcc9105c612ed6dcd8b66cce34a5b331393f52
Dmitry <mitroshin.dev@gmail.com>
2016-10-07 14:40:22 +0300

    CrimeActivity теперь использует универсальный класс SingleFragmentActivity для установки Фрагмента в макете activity_fragment

--- app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/devgmail/mitroshin/criminalintent/CrimeActivity.java
@@ -1,26 +1,11 @@
 package com.devgmail.mitroshin.criminalintent;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        // Передаем в fm фрагмент для управления
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            // Создает и закрепляет транзакцию фрагмента
-            // Т.е. указываем идентификатор контейнерного представления и экземпляра фрагмента
-            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
